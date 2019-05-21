d4602090cd9a77b054af702b2e9bdea4ca60ed5b
Maxim Belsky <public.belsky@gmail.com>
2019-05-21 21:56:17 +0300

    Remove imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { ThreadsController } from './threads/threads.controller';
 import { ThreadsModule } from './threads/threads.module';
 
 @Module({
